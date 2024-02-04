eaf79583cfb9874edd11c34aace8accdaedadc79
TranX91 <157170977+TranX91@users.noreply.github.com>
2024-02-03 18:23:15 -0600

    Update telephone.py

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,19 @@
+#Nhi Tran
+
 def telephone():
-    phone_number = int(input())
-    ''' Type your code here. '''
+    phone_number = int(input("Enter a phone number:" ))
+
+    line_number = phone_number % 10000
+    line_number =  "-" + str(line_number)
+    phone_number = phone_number // 10000
+
+    prefix = phone_number % 1000
+    prefix = " " + str(prefix)
+    phone_number = phone_number // 1000
+    
+    area_code = "(" + str(phone_number) + ")"
+
+    final = area_code + prefix + line_number 
+    print(final)
 if __name__ == "__main__":
     telephone()
