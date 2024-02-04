404726934c07713abc88e19a32130e6a25075afe
TranX91 <157170977+TranX91@users.noreply.github.com>
2024-02-03 18:47:49 -0600

    Update caffeine.py

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,7 +1,17 @@
+#Nhi Tran
 
 def caffeine():
-    caffeine_mg = float(input())
-    ''' Type your code here. '''
+    caffeine_mg = float(input("mg of caffeine in coffee: "))
     
+    caffeine_mg = caffeine_mg / 2
+    print("after 6 hours: " + f'{caffeine_mg:.2f}')
+    
+    caffeine_mg = caffeine_mg / 2
+    print("after 12 hours: " +f'{caffeine_mg:.2f}')
+
+    caffeine_mg = caffeine_mg / 4
+    print("after 24 hours: " + f'{ caffeine_mg:.2f}')
+
+
 if __name__ == "__main__":
     caffeine()
